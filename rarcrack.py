b995997cdaf716fd0cfe4c5caff0183f8ec487df
Cherrot Luo <cherrot+github@cherrot.com>
2013-05-03 23:14:18 +0800

    nice finally

--- rarcrack.py
+++ rarcrack.py
@@ -13,11 +13,11 @@ except Exception:
 #with RarFile(sys.argv[1]) as rarFile:
 for i in range(2,len(sys.argv)):
     for filename in glob.glob(sys.argv[i]):
-        if lastFile and filename != lastFile:
+        if lastFile and filename != lastFile[:-1]:
             continue
 
         with open(filename) as dictFile:
-            dictFile.seek(offset)
+            dictFile.seek(int(offset))
             count = 0
             print('Reading password from {}'.format(sys.argv[i]))
             while True:
@@ -29,21 +29,19 @@ for i in range(2,len(sys.argv)):
                         pass
                     break
 
-                try:
-                    #rarFile.extractall(pwd=password[:-1])
-                    a = os.popen("unrar t -y -p{} {} 2>&1 | grep 'All OK'".format(
-                        password[:-1], sys.argv[1]))
-                    for i in a.readlines():
-                        if i == 'All OK\n':
-                            print('密码已破解：{}'.format(password))
-                            sys.stderr.write('密码已破解：{}'.format(password))
-                            exit(0)
+                #rarFile.extractall(pwd=password[:-1])
+                a = os.popen("unrar t -y -p{} {} 2>&1 | grep 'All OK'".format(
+                    password[:-1], sys.argv[1]))
+                for i in a.readlines():
+                    if i == 'All OK\n':
+                        print('密码已破解：{}'.format(password))
+                        sys.stderr.write('密码已破解：{}'.format(password))
+                        exit(0)
 
-                except Exception:
-                    count += 1
-                    if count == 100:
-                        print('Current : {}'.format(password))
-                        with open('{}.log'.format(sys.argv[1]), 'w',
-                                encoding='utf-8') as log:
-                            log.write('{}\n{}'.format(filename,dictFile.tell()))
-                    continue
+                count += 1
+                if count == 100:
+                    count = 0
+                    print('Current : {}'.format(password))
+                    with open('{}.log'.format(sys.argv[1]), 'w',
+                            encoding='utf-8') as log:
+                        log.write('{}\n{}'.format(filename,dictFile.tell()))
